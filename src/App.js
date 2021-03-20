3c6afaec5500d7dbbf64bbd4decdde113acc3495
michaeltheo <michaeltheochar@gmail.com>
2021-03-20 14:36:41 +0200

    to do list component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 //Importing Components
 import Form from "./Components/Form";
+import TodoList from "./Components/Todolist";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>To Do App</h1>
+        <h1>MIKE TO DO LIST</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
---
+++ src/Components/Todolist.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Todolist = () => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+  );
+};
+
+export default Todolist;
